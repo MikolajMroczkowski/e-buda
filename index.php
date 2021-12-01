1a88185f2c665687e389f1317ea13958dada35d2
Mikołaj Mroczkowski <mikolaj.mroczkowski@gmail.com>
2021-12-01 23:46:42 +0100

    Upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/own/css/reset.css">
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/own/css/menu.css">
+    <link rel="stylesheet" href="assets/own/css/main.css">
+    <title>aa</title>
+</head>
+<body>
+<div class="nav">
+    <h1>e-buda</h1>
+    <a class="active">O mnie</a>
+    <a class="">Aplikacje</a>
+    <a class="">Strony www</a>
+    <a class="">Sociale</a>
+    <a class="">Kontakt</a>
+
+</div>
+<div class="content">
+
+</div>
+</body>
+</html
